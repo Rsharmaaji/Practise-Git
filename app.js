e01eee01af5eb84d6b2f067fed52131034f4559b
Rsharmaaji <rsharmaaji@gmail.com>
2020-07-04 11:36:05 +0530

    Hello Vivek

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use("/api/users",userRouter)
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header(" Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "GET,PUT,PATCH,DELETE,POST");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
